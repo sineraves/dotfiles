f868779c5c7a89a3a1ebcd04bcfc8282a53ac82e
Matt Johnson <matt@heavyperil.com>
2019-06-16 15:34:07 +0100

    Update install list.

--- install.sh
+++ install.sh
@@ -11,12 +11,16 @@ chsh -s /usr/local/bin/zsh
 env zsh
 
 echo "Installing homebrew packages"
-brew install direnv elixir fzf git heroku-toolbelt most openssl postgresql python rbenv reattach-to-user-namespace ripgrep ruby-build tmux tree vim zsh-history-substring-search
+brew install bat exa fd fzf git neovim node python rbenv reattach-to-user-namespace ripgrep tmux tree z
+brew tap heroku/brew && brew install heroku
 brew tap universal-ctags/universal-ctags && brew install --HEAD universal-ctags
 
 latest_mri_ruby=$(rbenv install -l | grep -v - | tail -1 | tr -d '[:space:]')
 echo "Installing ruby ${latest_mri_ruby}"
 rbenv install $latest_mri_ruby
 rbenv global $latest_mri_ruby
-gem install bundler reek rubocop scss_lint tmuxinator
+gem install bundler rubocop tmuxinator
 rbenv rehash
+
+echo "Installing zgen"
+git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
