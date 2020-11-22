9fb12c823f88cf16b725d2c8a3d9e07147877bd3
Matt Johnson <m-tt@hey.com>
2020-11-22 14:58:08 +0000

    Review install script.

--- install.sh
+++ install.sh
@@ -4,15 +4,26 @@ echo "Installing homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Installing homebrew packages"
-brew install asdf bat coreutils exa fd fzf gpg neovim postgresql@11 reattach-to-user-namespace ripgrep tmux tree z
-brew tap heroku/brew && brew install heroku
-brew tap universal-ctags/universal-ctags && brew install --HEAD universal-ctags
+brew install \
+  asdf \
+  bat \
+  exa \
+  fd \
+  fzf \
+  gpg \
+  python \
+  reattach-to-user-namespace \
+  ripgrep \
+  tmux \
+  tree
+brew tap heroku/brew && \
+  brew install heroku
+brew tap universal-ctags/universal-ctags && \
+  brew install --HEAD universal-ctags
 
 echo "Adding asdf plugins"
-asdf plugin-add erlang
-asdf plugin-add elixir
-asdf plugin-add nodejs
 asdf plugin-add ruby
+asdf plugin-add nodejs
 
-echo "Installing zgen"
-git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+echo "Importing the Node.js release team's OpenPGP keys to main keyring"
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
