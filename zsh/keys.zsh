6e37ded7b25b7503398c29db39e542de389d45dd
Matt Johnson <churn_canyon0x@icloud.com>
2022-04-12 17:39:55 +0100

    Remove zsh.

--- zsh/keys.zsh
+++
@@ -1,5 +0,0 @@
-WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
-bindkey "^U" backward-kill-line
-bindkey "^[[A" history-substring-search-up
-bindkey "^[[B" history-substring-search-down
-bindkey -e
--- zsh/prompt.zsh
+++
@@ -1,4 +0,0 @@
-autoload colors && colors
-
-source ~/.zsh/git.zsh
-source ~/.zsh/themes/hyperzsh.zsh-theme
