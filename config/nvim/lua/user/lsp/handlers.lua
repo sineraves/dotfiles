44939359f6d77dd217b1bddb544c50d816265106
Matt Johnson <churn_canyon0x@icloud.com>
2022-04-12 12:13:39 +0100

    Move lsp document formatting handler.

--- config/nvim/lua/user/lsp/handlers.lua
+++ config/nvim/lua/user/lsp/handlers.lua
@@ -83,6 +83,15 @@ M.on_attach = function(client, bufnr)
     client.resolved_capabilities.document_formatting = false
   end
 
+  if client.resolved_capabilities.document_formatting then
+    vim.cmd([[
+          augroup LspFormatting
+          autocmd! * <buffer>
+          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+          augroup END
+          ]])
+  end
+
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
--- config/nvim/lua/user/lsp/null-ls.lua
+++ config/nvim/lua/user/lsp/null-ls.lua
@@ -12,17 +12,6 @@ local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
   debug = false,
-  -- format on save
-  on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
-      vim.cmd([[
-            augroup LspFormatting
-            autocmd! * <buffer>
-            autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-            augroup END
-            ]])
-    end
-  end,
   sources = {
     diagnostics.credo,
     diagnostics.eslint.with({
